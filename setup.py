03b51c147b7ab1e1cc967a00e07935c616917b4c
kaka-zuumi <kazuumi@RainbowComputer.localdomain>
2025-07-16 01:01:02 -1000

    More markdown...

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
    author='Kazuumi Fujioka',
    author_email='kazuumi@hawaii.edu',
    url="https://github.com/kaka-zuumi/VENUSpy",
-   packages=['venuspy'],
+#  packages=['venuspy'],
+   packages=find_packages(include=['venuspy'],exclude=['images','examples']),  #same as name
    install_requires=['numpy', 'scipy', 'ase'], #external packages as dependencies
    scripts=[
             'cli.py',
